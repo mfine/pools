7732fab8a963f7a9a8dff5629edd476ebf9a69d8
Mark Fine <mark.fine@gmail.com>
2017-05-17 14:33:02 -0700

    register.

---
+++ register.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import boto3
+from botocore.exceptions import ClientError
+
+def main():
+    client = boto3.client('swf')
+    try:
+        client.register_domain(
+            name='loops',
+            workflowExecutionRetentionPeriodInDays='NONE',
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] != 'DomainAlreadyExistsFault':
+            raise
+    try:
+        client.register_workflow_type(
+            domain='loops',
+            name='workflow',
+            version='1.0',
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] != 'TypeAlreadyExistsFault':
+            raise
+    try:
+        client.register_activity_type(
+            domain='loops',
+            name='activity',
+            version='1.0',
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] != 'TypeAlreadyExistsFault':
+            raise
+
+if __name__ == "__main__":
+    main()
